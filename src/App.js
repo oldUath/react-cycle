cf899d4e5ec56dfb5c15305958f2d7a172223079
oldUath <haiyangdong666@gmail.com>
2020-11-11 15:04:16 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state={
+      n:0
+    }
+  };
+  add=()=>{
+    this.setState(
+      (state)=>({
+        n: state.n+1
+      })
+    )
+    // this.setState(
+    //   (state)=>({
+    //     n: state.n-1
+    //   })
+    // )
+  };
+  
+  shouldComponentUpdate(newProps,newState){
+    if(newState.n === this.state.n){
+      return false
+    }else{
+      return true
+    }
+  };
+  render(){
+    console.log('render了1次')
+    let message;
+    if(this.state.n%2 === 0){
+      message = <div>偶数</div>
+    }else{
+      message = <div>奇数</div>
+    }
+    return(
+      <>
+        <div>
+          n:{this.state.n}
+          <button onClick={this.add}>+1</button>
+        </div>
+        {message}
+
+        </>
+    )
+  }
+};
 
 export default App;
